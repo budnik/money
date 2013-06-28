bb2251d1fabdb6a14ed0f43562a7ba1bf698bf1b
Dmitriy Budnik <dmbu@ciklum.com>
2013-06-28 09:23:00 +0300

    empty app w/ req'd gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Money::Application.config.secret_key_base = 'c57a2fbf2c2ae70bc1c6433fe873bae85cf2195e795a15ef1623ad800a8152b41b91f9194d19dce09b9ae2fe1734e734a94c91b363f171674d48ab6724166c8f'
